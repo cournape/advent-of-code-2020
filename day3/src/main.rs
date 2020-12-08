18610a3581d1e2394b432031a302dc8cb494a9e9
David Cournapeau <cournape@gmail.com>
2020-12-08 23:50:48 +0900

    FEAT: correct soluce to day 3, part 1.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,16 +1,42 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+use array2d::Array2D;
+
+extern crate clap;
+use clap::{Arg, App};
+
+#[derive(Copy, Clone)]
 enum LocationKind{
 	Tree,
 	OpenSquare,
 }
 	
 fn main() {
-	let path = "./simple";
-	let n_trees = solve_main_problem(&path.to_string()).unwrap();
+	let matches = App::new("day 3")
+                          .arg(Arg::with_name("INPUT")
+                               .help("Sets the input file to use")
+                               .required(true)
+                               .index(1))
+                          .arg(Arg::with_name("part-two")
+			       .short("2")
+			       .long("part-two")
+                               .help("If set, solves part 2"))
+                          .get_matches();
 
-	println!("Encountered {} trees", n_trees);
+
+	let filename = matches.value_of("INPUT").unwrap().to_string();
+	println!("Using input file: {}", filename);
+
+	match matches.occurrences_of("part-two") {
+		0 => {
+			let n_trees = solve_main_problem(&filename).unwrap();
+			println!("Encountered {} trees", n_trees);
+		},
+		_ => {
+			panic!("Not supported yet");
+		}
+	}
 }
 
 fn solve_main_problem(filename: &String) -> Result<usize, &'static str> {
@@ -37,12 +63,26 @@ fn solve_main_problem(filename: &String) -> Result<usize, &'static str> {
 	let n_rows = data.len();
 	if n_rows > 0 {
 		let n_cols = data[0].len();
-		for row in data {
+		for row in &data {
 			if row.len() != n_cols {
 				panic!("Inconsistent grid");
 			}
 		}
 	}
 
-	Ok(0)
+	let grid = Array2D::from_rows(&data);
+
+	let mut j = 0;
+	let mut n_trees = 0;
+
+	for i in 0..grid.num_rows() {
+		let pos = grid[(i, j % grid.num_columns())];
+		match pos {
+			LocationKind::Tree => n_trees += 1,
+			LocationKind::OpenSquare => (),
+		}
+		j += 3;
+	}
+
+	Ok(n_trees)
 }
