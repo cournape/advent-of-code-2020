62d53c6f1740a947a2b2c0bebdbc91d52b6e7b16
David Cournapeau <cournape@gmail.com>
2020-12-09 20:02:15 +0900

    FEAT: day 4, part 1 implemented.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -12,26 +13,64 @@ fn main() {
                                .index(1))
                           .get_matches();
 
-
 	let filename = matches.value_of("INPUT").unwrap().to_string();
 	println!("Using input file: {}", filename);
 
-	parse_data(&filename);
+	let n_valid = solve_main_problem(&filename);
+
+	println!("Found {} valid entries", n_valid);
 }
 
-fn parse_data(filename: &String) -> () {
+fn is_valid_entry(data: &String) -> bool {
+	// We use a set instead of count to handle the case where a field may
+	// be repeated.
+	let mut fields = HashSet::new();
+
+	for part in data.split_whitespace() {
+		let pos = part.find(":").unwrap();
+		let field_name = &part[..pos];
+		match field_name {
+			"byr" | "iyr" | "eyr" | "hgt" | "hcl" | "ecl" | "pid" | "cid" => {
+				fields.insert(field_name);
+			},
+			_ => panic!("Unexpected field: {}", field_name),
+		};
+	}
+
+	fields.len() == 8 || (fields.len() == 7 && !fields.contains("cid"))
+}
+
+fn solve_main_problem(filename: &String) -> usize {
 	let file = File::open(filename).expect("failed to open");
 
 	let buf = io::BufReader::new(file);
 	let mut chunk = Vec::new();
 
+	let mut n_valid: usize = 0;
+
+	let mut process_data = |data| {
+		if is_valid_entry(&data) {
+			n_valid += 1;
+		} else {
+			println!("{}", data);
+		}
+	};
+
 	for line in buf.lines() {
 		let line = line.unwrap();
 		if line.trim().is_empty() {
-			println!("{}", chunk.join(" "));
+			let data = chunk.join(" ");
+			process_data(data);
 			chunk.clear();
 		} else {
 			chunk.push(line);
 		}
 	}
+
+	if chunk.len() > 0 {
+		let data = chunk.join(" ");
+		process_data(data);
+	}
+
+	n_valid
 }
