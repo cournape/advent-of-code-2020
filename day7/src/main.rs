c16e17cc24bc8342b72484a312c926b037bd3f06
David Cournapeau <cournape@gmail.com>
2020-12-10 18:27:07 +0900

    WIP

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate lazy_static;
 
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -9,7 +11,7 @@ extern crate clap;
 use clap::{Arg, App};
 
 fn main() {
-    let matches = App::new("day 6")
+    let matches = App::new("day 7")
         .arg(Arg::with_name("INPUT")
              .help("Sets the input file to use")
              .required(true)
@@ -35,22 +37,47 @@ fn main() {
 	}
 }
 
+// const MY_BAG : String = "shiny gold".to_string();
+
 fn solve_problem(filename: &String) -> () {
 	let file = File::open(filename).expect("failed to open");
 
 	let buf = io::BufReader::new(file);
 
+    let mut parent_to_children = HashMap::new();
+    let mut children_to_parents = HashMap::<String, Vec<String>>::new();
+
 	for line in buf.lines() {
 		let line = line.unwrap();
         let (parent, children) = parse_line(&line);
+        for child in children.keys() {
+            if !children_to_parents.contains_key(child) {
+                children_to_parents.insert(child.to_string(), vec![parent.clone()]);
+            } else {
+                children_to_parents[child].push(parent);
+            }
+        }
+        parent_to_children.insert(parent, children);
     }
+
+    // println!("Found {} nodes", nodes.len());
+    // let mut visited = HashSet::new();
+    // let mut ancestors = HashSet::new();
+
+    // while true {
+    //     for (parent, children) in graph {
+    //         if children.contains_key(parent) {
+    //             ancestors.insert(parent)
+    //         }
+    //     }
+    // }
 }
 
 const BAG_CONTAIN: &str = "bags contain";
 
-fn parse_line(line: &String) -> (String, Vec<String>) {
+fn parse_line(line: &String) -> (String, HashMap<String, i32>) {
 	lazy_static! {
-		static ref CHILD_BAG_RE: Regex = Regex::new(r"\d+ (\w+ \w+) bag").unwrap();
+		static ref CHILD_BAG_RE: Regex = Regex::new(r"(\d+) (\w+ \w+) bag").unwrap();
 	}
 
     let pos = line.find(BAG_CONTAIN).unwrap();
@@ -58,11 +85,11 @@ fn parse_line(line: &String) -> (String, Vec<String>) {
     let left_part = &line[..pos].trim();
     let right_part = line[pos + BAG_CONTAIN.len()..].trim();
 
-    let mut children = Vec::<String>::new();
+    let mut children = HashMap::<String, i32>::new();
 
     if right_part != "no other bags." {
         for cap in CHILD_BAG_RE.captures_iter(right_part) {
-            children.push((&cap[1]).to_string());
+            children.insert((&cap[2]).to_string(), cap[1].parse().unwrap());
         }
     }
 
